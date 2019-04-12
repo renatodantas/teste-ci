b306ca521ee24532d7202318a6bfa0ea455d94cc
Renato Dantas <renato.dantas@tst.jus.br>
2019-04-12 15:20:21 -0300

    Primeiro build compilável na imagem Docker

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "[ATENÇÃO] Execute o comando com o nome do projeto."
+    echo "Ex.: ./build ci-modulo-1"
+    exit 1
+fi
+
 docker run -it --rm \
-    -v "$PWD"/ci-modulo-dependencia:/usr/src/build \
-    -v "$PWD"/docker/settings.xml:/usr/share/maven/ref/settings.xml \
+    --network=teste-ci_default \
+    -v $PWD/$1:/usr/src/build \
+    -v $PWD/docker/settings.xml:/usr/share/maven/ref/settings.xml \
     -w /usr/src/build \
     maven:3.3-jdk-8-alpine \
-    mvn clean install
+    mvn clean install --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
