95dddb243c9f3b01057477fb29d6292eb00f0c28
Renato Dantas <renato.dantas@tst.jus.br>
2019-04-15 17:03:51 -0300

    Inclusão de gitlab-ci.yml

--- create_repositories.sh
+++ create_repositories.sh
@@ -2,21 +2,19 @@
 
 read -rsp $'Pressione <Enter> para criar os repositorios no GitLab ou <Ctrl+C> para abortar...\n'
 
-declare -a projetos=("ci-modulo-dependencia" "ci-modulo-1" "ci-modulo-2")
 DIR_ATUAL=$PWD
-
-for projeto in "${projetos[@]}"; do
-    echo "==> Criando repositorio: $i"
-    cd $DIR_ATUAL/$i
-    #git init
-    #echo target >> .gitignore
-    #echo .classpath >> .gitignore 
-    #echo .project >> .gitignore 
-    #echo .settings >> .gitignore 
-    #git add .
-    #git commit -m "Primeiro commit"
-    #git push --set-upstream http://localhost:9002/root/$i.git master
-    
+for projeto in ci-modulo-dependencia ci-modulo-1 ci-modulo-2; do
+    echo "==> Criando repositorio: $projeto"
+    cd $DIR_ATUAL/$projeto
+    git init
+    rm .gitignore
+    echo target >> .gitignore
+    echo .classpath >> .gitignore 
+    echo .project >> .gitignore 
+    echo .settings >> .gitignore 
+    git add .
+    git commit -m "Primeiro commit"
+    git push --set-upstream http://localhost:9002/root/$projeto.git master
 done
 
 echo "==> Finalizado"
